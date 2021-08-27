e6ffc9c10c20ee0e265681518c563a71fdefe9ad
Vagmpo <vaggelisbolofis@gmail.com>
2021-08-27 11:51:14 +0200

    #

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
+//
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <p>this is also visible</p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
